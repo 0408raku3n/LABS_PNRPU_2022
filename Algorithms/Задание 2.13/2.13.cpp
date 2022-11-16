25142ef3c9cecd606e0253b510f454231c96fc1a
Daniil Hamidullin <115153073+0408raku3n@users.noreply.github.com>
2022-11-16 15:19:44 +0500

    Update 2.13.cpp

--- Algorithms/Задание 2.13/2.13.cpp
+++ Algorithms/Задание 2.13/2.13.cpp
@@ -7,10 +7,11 @@ int main()
 {
 	float a, b, c; int D;
 	
+	cout << "Введите коэффициенты a, b, c" << endl;
 	cin >> a >> b >> c;
 
 	D = sqrt(b * b - 4 * a * c);
 
-	cout << ((b * (-1) + D) / (2 * a)) << endl;
-	cout << ((b * (-1) - D) / (2 * a)) << endl;
+	cout << "Первый корень равен:" << ((b * (-1) + D) / (2 * a)) << endl;
+	cout << "Второй корень равен:" << ((b * (-1) - D) / (2 * a)) << endl;
 }
