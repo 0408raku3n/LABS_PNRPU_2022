e262bf7c3e9e7c5a2ec7a77e6a444cdc831a0929
Daniil Hamidullin <115153073+0408raku3n@users.noreply.github.com>
2023-05-03 15:17:05 +0500

    Add files via upload

---
+++ Algorithms/Лабораторные/Классы/Лабораторная №9/Лабораторная №9.1/Source.cpp
@@ -0,0 +1,29 @@
+#include "./Vector.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try
+	{
+		Vector x(2);
+		Vector y;
+		cout << x;
+		cout << "\nEnter number: ";
+		int i;
+		cin >> i;
+
+		cout << x[i] << endl;
+		cout << x() << endl;
+		--x;
+		cout << x;
+		--x;
+		cout << x;
+		x--;
+	}
+	catch(int)
+	{
+		cout << "\n\nERROR!!!" << endl;
+	}
+	return 0;
+}
